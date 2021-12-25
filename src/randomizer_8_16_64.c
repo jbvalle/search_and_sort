da5cd0d270e2d89f19e018ead0fa832687bcc023
John Bryan Valle <jbvalle.jv@gmail.com>
2021-12-25 08:34:43 +0100

    changed formating of output

--- src/randomizer_8_16_64.c
+++ src/randomizer_8_16_64.c
@@ -52,9 +52,11 @@ void show_8_16_64(int **input){
         exp += i;
         for(int j = 0; j < pow(2,exp);j++){
             
-            printf("\n");
-            if(j%14 == 0)for(int i = 0; (i < 14) && (i < pow(2,exp)); i++)printf("+------+");
-            printf("\n");
+            if(j%14 == 0){
+                printf("\n");
+                for(int i = 0; (i < 14) && (i < pow(2,exp)); i++)printf("+------+");
+                printf("\n");
+            }
             printf("|%6d|", *(input[i] + j));
         }
         printf("\n");
