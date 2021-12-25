92e7b5c58c108691b242a133974b5780508c3fc7
John Bryan Valle <jbvalle.jv@gmail.com>
2021-12-25 19:22:04 +0100

    implemented bubblesort module with double pointer as input

---
+++ include/bubblesort.h
@@ -0,0 +1,14 @@
+/** .
+ * @file bubblesort.h
+ * for doxy
+ *
+ *
+ */
+
+#ifndef BUBBLESORT_H
+#define BUBBLESORT_H
+
+
+void bubblesort(int **random_array, int sizeof_array);
+
+#endif
--- src/bubblesort.c
+++ src/bubblesort.c
@@ -9,7 +9,9 @@
 #include <stdlib.h>
 #include "../include/search_index.h"
 
-void bubblesort(int *random_array, int sizeof_array){
+void bubblesort(int **random_array, int sizeof_array){
+
+  int index = search_index(sizeof_array);
 
   for (int i = 0; i < sizeof_array-1; i++) {
 
@@ -17,11 +19,11 @@ void bubblesort(int *random_array, int sizeof_array){
 
     for (int j = 0; j < sizeof_array-1; j++) {
 
-      if (random_array[j] > random_array[j + 1]){
+      if (*(random_array[index] + j) > *(random_array[index] + (j + 1))){
 
-        int initial_value_buffer = random_array[j];
-        random_array[j] = random_array[j + 1];
-        random_array[j + 1] = initial_value_buffer;
+        int initial_value_buffer = *(random_array[index] + j);
+        *(random_array[index] + j) = *(random_array[index] + (j + 1));
+        *(random_array[index] + (j + 1)) = initial_value_buffer;
         allignment_flag = 1;
       }
     }
