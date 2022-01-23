191ecc66b26f8ea223da5fa868438057a2d87353
John Bryan Valle <jbvalle.jv@gmail.com>
2022-01-23 06:47:21 +0100

    added insertion_sort, performance test 1.3 Martin, implemented functions in menue

--- src/testing.c
+++ src/testing.c
@@ -20,87 +20,14 @@
 #include "../include/search_index.h"
 #include "../include/quicksort.h"
 #include "../include/testing.h"
+#include "../include/bubblesort_timing_pt.h"
+#include "../include/insertion_sort_arr_ll.h"
 
 /** @brief Define \c stdout Color */
 #define COLOR "\033[1;31m"
 #define RESET "\033[0m"
 
-void append_node(node_t **head, int num){
 
-    node_t *ptr = *head;
-
-    node_t *new = malloc(sizeof(node_t));
-    new->num = num;
-    new->next = NULL;
-
-    if(*head == NULL){
-
-        *head = new;
-        return;
-    }
-
-    for(; ptr->next != NULL; ptr = ptr->next);
-    
-    ptr->next = new;
-}
-
-void populate_list(node_t **head, int *arr, int size){
-
-   for(int i = 0; i < size; i++)append_node(head, arr[i]); 
-
-}
-
-void sorted_insert(node_t **sorted, node_t *new){
-
-
-    if((*sorted) == NULL || ((*sorted)->num) >= new->num){
-
-        new->next = *sorted;
-        *sorted = new;
-    }else{
-
-        node_t *curr_ptr = *sorted;
-
-        while(curr_ptr->next != NULL && (curr_ptr->next->num <= new->num)){
-            
-            curr_ptr = curr_ptr->next;
-        }
-
-        new->next = curr_ptr->next;
-        curr_ptr->next = new;
-    }
-}
-
-
-void insertionSort(node_t **head){
-
-    node_t *sorted = NULL;
-
-    node_t *curr_ptr = *head;
-
-    while(curr_ptr != NULL){
-
-        node_t *temp = curr_ptr->next;
-
-        sorted_insert(&sorted, curr_ptr);
-        
-        curr_ptr = temp;
-    }
-
-    *head = sorted;
-}
-
-void free_list(node_t *head){
-
-    node_t *ptr = head;
-
-    while(ptr != NULL){
-
-        node_t *temp = ptr->next;
-        free(ptr);
-        ptr = temp;
-    }
-}
 /** @brief Function to create and call free dyn. memory as well as call subfunctions for performance test. 
  * @param[in] arg1 First Array Element to be swapped
  * @param[in] arg2 Second Array Element to be swapped
@@ -123,7 +50,10 @@ void testing(){
     randomizer_8_16_64(input);
      //Show array elements
     show_8_16_64(input, 10); //To display remove Backslashes
-    
+   
+    //==============
+    //Bubblesort
+    //==============
     randomizer_8_16_64(input);
     printf("\n\n");printf(COLOR);printf("+----------------------+\n");
     printf(COLOR);printf("| Sorted by Bubblesort |\n");
@@ -190,61 +120,18 @@ void testing(){
     //Free input arrays
     free_8_16_64(input);
 
+    //Perform Insertion Sort Performance Test Array List
+    insertionsort_arr_ll();
 
-    //===================================================
-    //Insertionsort ARRAYS-LINKED-LIST Perfomance test
-    //===================================================
-    //Saves CPU Runtime value of a single function
-    double pt_cpu_time_sec[2] = {0, 0};
+    //Performs bubblesort algorithm on a generated randomized array of size 2000
+    //Subsequently sorting the same set of numbers 20 times and comparing cpu time
+    int randomized[2000];
 
-    //Save temporary clocks at current time
-    clock_t cpu_time_clk;
+    srand(time(NULL));
 
-    //Allocate HEAD Node for Linked list
-    node_t *head = NULL;
-    //Allocate Memory for Randomized ARRAY
-    int **input_pt = (int **)malloc(7 * sizeof(int *));
-    randomizer_pt(input_pt);
-
-    //Fill linked list with values of randomized list of size 2048
-    populate_list(&head, input_pt[search_index(2048)], 2048);
-
-    //==============================================
-    // INSERTION SORT FOR LINKED LIST 
-    //==============================================
-    //Measure Clock at current time
-    cpu_time_clk = clock();
-    //Perform Insertionsort for Linked List
-    insertionSort(&head);
-    //Measure Clock at current time
-    cpu_time_clk = clock();
-    pt_cpu_time_sec[0] = (double)cpu_time_clk/(double)CLOCKS_PER_SEC;
-    //==============================================
-
-    //==============================================
-    // INSERTION SORT FOR ARRAY
-    //==============================================
-    //Measure Clock at current time
-    cpu_time_clk = clock();
-    //Perform Insertionsort for Array
-    insertionsort(input_pt, 2048);
-    //Measure Clock at current time
-    cpu_time_clk = clock();
-    pt_cpu_time_sec[1] = (double)cpu_time_clk/(double)CLOCKS_PER_SEC;
-    //==============================================
-
-    //Show list for linked list
-    show_randomizer_pt_list(head, search_index(2048),15);
-
-    //Show list of array
-    show_randomizer_pt_index(input_pt, search_index(2048),15);
-    
-    printf("\033[1;93m");
-    printf("\n\n+-----------------------------------------------------------------+");
-    printf("\n| RUNTIME-LINKED-LIST[ms]: %2.3fms | RUNTIME-ARRAY[ms]: %2.3fms |", pt_cpu_time_sec[0]*1000., pt_cpu_time_sec[1]*1000.);
-    printf("\n+-----------------------------------------------------------------+\n");
-    printf("\033[0m");
-
-    free_pt(input_pt); 
-    free_list(head);
+    for(int x = 0; x<2000; x++)
+    {
+        randomized[x] = (double)65536*((double)rand()/(double)RAND_MAX) - 32768.;
+    }
+    bubblesort_timing_pt(randomized);
 }
